c46b4400607d49af84b0de7bcd01cfdc62704d6d
Shana2002 <hansakaravi02@gmail.com>
2025-01-08 21:06:48 +0530

    update program details

--- src/controllers/program.js
+++ src/controllers/program.js
@@ -19,7 +19,22 @@ export const updateProgram = (req,res) =>{
     checkToken(req,res,'secretkeySuperAdmin',(err,userInfo)=>{
         if (err) return res.status(400).json(err.message);
 
+        const {title,description,program_characters,duration} = req.body;
+
+        // fileds to update 
+        const filedsToUpdate = {}
+
+        if (title) filedsToUpdate.title = title;
+        if (description) filedsToUpdate.description = description;
+        if (program_characters) filedsToUpdate.program_characters = program_characters;
+        if (duration) filedsToUpdate.duration = duration;
         
+        const q = "UPDATE program SET ? WHERE program_id = ?";
+        db.query(q,[filedsToUpdate,req.body.programId],(err,data)=>{
+            if (err) return res.status(500).json(err);
+
+            return res.status(200).json("Data update successfull");
+        })
     })
 }
 
